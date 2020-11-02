a13fac2a980cd5e3d6b7b0fcf51b437b860c1c8e
Aleksandr Potkin <apotkin@xdsoft.ru>
2020-11-02 17:43:21 +0300

    note for pay

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+function App() {
+
+const consumers = ['Череповец', 'Санкт-Петербург', 'Новгород' ]
+
+const monthsArr = [
+    'Январь', 'Февраль', 'Март', 'Апрель',
+    'Май', 'Июнь', 'Июль', 'Август',
+    'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+];
+
+const monthsArrEnd = [
+  'Января', 'Февраля', 'Марта', 'Апреля',
+  'Мая', 'Июня', 'Июля', 'Августа',
+  'Сентября', 'Октября', 'Ноября', 'Декабря'
+];
+
+const now = new Date();
+const currentMonth = 10;
+
+const whoPay = currentMonth % consumers.length  
+
+let prevMonth;
+
+if(currentMonth === 0) {
+  prevMonth = 11
+} else {
+  prevMonth = currentMonth - 1
+}
+
+const onCopy = () => {
+  navigator.clipboard.writeText('+79992349352')
+}
+
+  return (
+    <div className="App">
+      <p>Период: 14 {monthsArrEnd[prevMonth]} - 14 {monthsArrEnd[currentMonth]} {now.getFullYear()}</p>
+      <p>Оплачивает: {consumers[whoPay]}</p>
+     
+      <table>
+        {monthsArr.map((m, index) => <tr key={index} className={`${index === currentMonth && 'active'}`}>
+          <td>{m}</td><td>{consumers[index % consumers.length]}</td>
+          
+          </tr>)}
+      </table>
+
+      <p>Стоимость подписки: 299р</p>
+      <p>+79992349352</p>
+      <button onClick={onCopy}>Скопировать телефон</button>
+    </div>
+  );
+}
+
+export default App;
